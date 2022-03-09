5502c3cdbe065e8e508da23a0a6921f5e626306b
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-03-09 15:10:37 +0700

    Add navbar transition

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+let $ = document.querySelector.bind(document);
+let $$ = document.querySelectorAll.bind(document);
+
+let navbar = $('.navbar');
+navbar.addEventListener('click', () => {
+    console.log("yes")
+})
+
+let navbarScrollStyle = `
+    .navbar {
+        background-color: white;
+        color: black;
+    }
+
+    .navbar__logo {
+        color: #ff395c;
+    }
+
+    .navbar__globe_svg {
+        color: black;
+    }
+
+    .navbar__item-link {
+        color: black;
+    }
+
+    .navbar__search-wrapper {
+        transform: translateY(-80px) scale(0.4);
+        opacity: 0;
+     
+    }
+
+    .navbar__search--collapse {
+        display: block;
+        opacity: 1;
+        top: 20px;
+
+    }
+
+    .navbar__center-nav {
+        visibility: hidden;
+    }
+
+`
+let styleNode = document.createElement('style')
+styleNode.innerText = navbarScrollStyle
+
+window.onscroll = () => {
+    console.log("-------")
+    console.log(Math.floor(window.scrollY))
+    if (window.scrollY !== 0) {
+        document.head.appendChild(styleNode)
+    } else {
+        document.head.removeChild(styleNode)
+    }
+}
